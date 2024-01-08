3daf1e6ef0dc1339829f9dabac1b07623f18792c
Om Bharti <ombharti888@gmail.com>
2024-01-09 00:00:57 +0530

    ui layout update

--- app/_component/Header/Header.tsx
+++ app/_component/Header/Header.tsx
@@ -3,10 +3,19 @@ import "./index.css";
 import Link from "next/link";
 import Image from "next/image";
 
-export function Header() {
+interface Props {
+  homepage: boolean;
+}
+
+export function Header({ homepage }: Props) {
   return (
-    <div className="header">
-      <Image src={"/assets/image/airplane-logo.jpg"} width={24} height={24} alt="logo" />
+    <div className={`header ${homepage ? "absolute z-10 w-8/12 rounded-md" : ""}`}>
+      <Image
+        src={"/assets/image/airplane-logo.jpg"}
+        width={24}
+        height={24}
+        alt="logo"
+      />
       <div className="header-link">
         <Link href={"./home"}>Home</Link>
         <Link href={"./about"}>About</Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 // import Image from "next/image";
 
+import { Header } from "./_component";
+
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <video loop autoPlay muted style={{ width: "100%", height: "auto" }}>
+    <main className="min-h-screen w-full">
+      <Header homepage/>
+      <div className="absolute top-0 w-full h-full bg-[#0000004d]"></div>
+      <video loop autoPlay muted style={{ width: "100%", height: "100vh", objectFit:"cover" }}>
         <source src="/assets/video/mountain_view.mp4" />
       </video>
+      <div className="absolute w-full h-full flex top-0 flex-col justify-center items-center font-bold text-white">
+        <h1 className="text-7xl ">Welcome</h1>
+        <h2 className="text-6xl">to my site</h2>
+      </div>
     </main>
   );
 }
