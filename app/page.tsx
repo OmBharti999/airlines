0808b795a5bfe9d51cd089f25f422333c5df67d6
Om Bharti <ombharti888@gmail.com>
2024-01-08 23:34:52 +0530

    add video to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+// import Image from "next/image";
 
 export default function Home() {
   return (
-    <main >
-hi
+    <main className="min-h-screen">
+      <video loop autoPlay muted style={{ width: "100%", height: "auto" }}>
+        <source src="/assets/video/mountain_view.mp4" />
+      </video>
     </main>
-  )
+  );
 }
