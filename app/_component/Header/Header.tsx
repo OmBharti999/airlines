a0d759b388547ae80ff386f3e7808189926c7643
Om Bharti <ombharti888@gmail.com>
2024-01-08 23:07:25 +0530

    1. add logo 2. add header

---
+++ app/_component/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+import Link from "next/link";
+import Image from "next/image";
+
+export function Header() {
+  return (
+    <div className="header">
+      <Image src={"/airplane-logo.jpg"} width={24} height={24} alt="logo" />
+      <div>
+        <Link href={"./home"}>Home</Link>
+        <Link href={"./about"}>About</Link>
+        <Link href={"./contact"}>Contact</Link>
+      </div>
+    </div>
+  );
+}
