4ddea14a77e995717b25f4f6953b409c718868f8
Om Bharti <ombharti888@gmail.com>
2024-01-08 23:26:55 +0530

    add imge and video for homepage

--- app/_component/Header/Header.tsx
+++ app/_component/Header/Header.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export function Header() {
   return (
     <div className="header">
-      <Image src={"/airplane-logo.jpg"} width={24} height={24} alt="logo" />
+      <Image src={"/image/airplane-logo.jpg"} width={24} height={24} alt="logo" />
       <div className="header-link">
         <Link href={"./home"}>Home</Link>
         <Link href={"./about"}>About</Link>
